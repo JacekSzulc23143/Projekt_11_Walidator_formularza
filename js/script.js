c8cc74726becf750080e28e5ab23636905e3a99a
Jacek Szulc <j.szulc77@wp.pl>
2024-09-08 15:28:56 +0200

    Projekt#11.3

--- js/script.js
+++ js/script.js
@@ -6,6 +6,39 @@ const clearBtn = document.querySelector(".clear");
 const sendBtn = document.querySelector(".send");
 const popup = document.querySelector(".popup");
 
+// funkcja pokazująca error
+const showError = (input, msg) => {
+	// argument INPUT przechowuje nasze INPUTy
+	// argument MSG przechowuje placeholder
+
+	const formBox = input.parentElement;
+	const errorMsg = formBox.querySelector(".error-text");
+
+	formBox.classList.add("error");
+	errorMsg.textContent = msg;
+};
+
+// funkcja usuwająca error
+const clearError = input => {
+	const formBox = input.parentElement;
+	formBox.classList.remove("error");
+};
+
+// funkcja sprawdzająca formularz
+// argument INPUT z funkcji "checkForm" przechowuje tablicę z naszymi inputami
+// argument EL odnosi się do każdej zmiennej, którą umieściliśmy w tablicy
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === "") {
+			// console.log(el);
+			// console.log(el.placeholder);
+			showError(el, el.placeholder);
+		} else {
+			clearError(el);
+		}
+	});
+};
+
 // przycisk Wyczyść
 clearBtn.addEventListener("click", e => {
 	e.preventDefault();
@@ -13,4 +46,11 @@ clearBtn.addEventListener("click", e => {
 	[username, pass, pass2, email].forEach(el => {
 		el.value = "";
 	});
+});
+
+// przycisk Wyślij
+sendBtn.addEventListener("click", e => {
+	e.preventDefault();
+
+	checkForm([username, pass, pass2, email]);
 });
