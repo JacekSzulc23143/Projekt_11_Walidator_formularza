6c830d516fec3a3755ceeedcc0d5bc3fe4b6b4f7
Jacek Szulc <j.szulc77@wp.pl>
2024-09-08 17:18:32 +0200

    Projekt#11.5

--- js/script.js
+++ js/script.js
@@ -53,9 +53,21 @@ const checkLength = (input, min) => {
 };
 
 // funkcja sprawdzająca zgodność haseł w obydwu polach input
-const checkPassword = (pass, pass2) => {
-	if (pass.value !== pass2.value) {
+const checkPassword = (pass1, pass2) => {
+	if (pass1.value !== pass2.value) {
 		showError(pass2, "Hasła do siebie nie pasują.");
+	}
+};
+
+// funkcja sprawdzająca poprawność składni adresu e-mail
+const checkMail = email => {
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	if (re.test(email.value)) {
+		clearError(email);
+	} else {
+		showError(email, "e-mail jest niepoprawny");
 	}
 };
 
@@ -76,4 +88,5 @@ sendBtn.addEventListener("click", e => {
 	checkLength(username, 3);
 	checkLength(pass, 8);
 	checkPassword(pass, pass2);
+	checkMail(email);
 });
